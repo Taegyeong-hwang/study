72ede7517d435b6f0601f62b24cf505175b965fb
Taegyeong-hwang <alainplaire@naver.com>
2017-09-04 16:07:26 +0900

    nothing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 // 걸프 의존선을 여기 씁니다.
 
 gulp.task('default',function(){
-	// 걸프 작업을 여기 씁니다.
+	gulp.src("es6/**/**,js")
+	.pipe(babel())
+	.pipe(gulp.dest("dist"));
+	// 브라우저 소스
+	
+	gulp.src("public.es6/**/*.js")
+	.pipe(babel())
+	.pipe(gulp.dest("public/dist"));
 });
